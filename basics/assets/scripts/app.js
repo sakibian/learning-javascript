a0d635852239083b0720562014b83ff715dce690
Sakib <sakib192@gmail.com>
2021-04-23 14:40:47 +0600

    starting with basic

---
+++ basics/assets/scripts/app.js
@@ -0,0 +1,16 @@
+// Un-initialize variables. 
+// let currentResult;
+
+// Initialize variables.
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1 ;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
+
+
+
