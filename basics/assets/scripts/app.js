9d2fbce8eb8e1eb820fa342423e229067bea4d39
Sakib <sakib192@gmail.com>
2021-04-26 11:48:17 +0600

    Learning functions

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,21 +1,48 @@
-// Un-initialize variables. 
-// let currentResult;
-
-// Initialize variables.
 const defaultResult = 0;
-// Global variable
 let currentResult = defaultResult;
 
-// + Add function
+function getUserNumberInput() {
+  // another way to convert (+ +userInput.value )
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutPut(operator, resultBeforeCalc, calcNumber){
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
 function add() {
-  // Global variable initialize
-  currentResult = currentResult + parseInt(userInput.value); // another way to convert + +userInput.value
-  // Output Results
-  outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult; 
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutPut('+', initialResult, enteredNumber);
+}
+
+function substract(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult; 
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutPut('-', initialResult, enteredNumber);
+}
+
+function multiply(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult; 
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutPut('*', initialResult, enteredNumber);
+}
+
+function divide(){
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult; 
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutPut('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
-
+subtractBtn.addEventListener('click', substract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
 
